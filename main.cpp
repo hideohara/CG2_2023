ee3f6956efb4849e4a9d240d28083f8f396d4699
oharakogakuin <ohara@g.neec.ac.jp>
2023-10-17 09:28:54 +0900

    00_04 ログを出そう

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <Windows.h>
 #include <cstdint>
+#include <string>
+#include <format>
 
-// 検証用
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+// string->wstring
+std::wstring ConvertString(const std::string& str);
+// wstring->string
+std::string ConvertString(const std::wstring& str);
+
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
 
 
 // ウィンドウプロシージャ
@@ -26,6 +63,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// 出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
+
+	// wstring->string
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"AAAA")));
 
 
 	WNDCLASS wc{};
