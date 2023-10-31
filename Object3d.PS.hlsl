ca44556089a3bd9599af1d0627df95ddecc87fa0
oharakogakuin <ohara@g.neec.ac.jp>
2023-10-31 12:52:42 +0900

    02_00_資料_PhongReflectionModel

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -11,9 +11,14 @@ struct DirectionalLight
     float32_t3 direction; //!< ライトの向き
     float intensity; //!< 輝度
 };
+struct Camera
+{
+    float32_t3 worldPosition;
+};
 
 ConstantBuffer<Material> gMaterial : register(b0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+ConstantBuffer<Camera> gCamera : register(b2);
 
 struct PixelShaderOutput
 {
@@ -25,8 +30,26 @@ PixelShaderOutput main(VertexShaderOutput input)
     //output.color = gMaterial.color;
     if (gMaterial.enableLighting != 0)
     { // Lightingする場合
+        float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+        float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        float RdotE = dot(reflectLight, toEye);
+        float specularPow = pow(saturate(RdotE), 50); // 反射強度
+
         float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
-        output.color = gMaterial.color * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        //output.color = gMaterial.color * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    
+        // 拡散反射
+        float32_t3 diffuse =
+        gMaterial.color.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        // 鏡面反射
+        float32_t3 specular =
+        gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
+        // 拡散反射+鏡面反射
+        output.color.rgb = diffuse + specular;
+        // アルファは今まで通り
+        output.color.a = gMaterial.color.a;
+
+
     }
     else
     { // Lightingしない場合。前回までと同じ演算
