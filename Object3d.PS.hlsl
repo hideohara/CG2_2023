72a3c2804f5c675af4e948eee611369ca5f18407
oharakogakuin <ohara@g.neec.ac.jp>
2023-10-31 12:01:01 +0900

    04_03_資料_LambertianReflectance_完成

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -22,7 +22,17 @@ struct PixelShaderOutput
 PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
-    output.color = gMaterial.color;
+    //output.color = gMaterial.color;
+    if (gMaterial.enableLighting != 0)
+    { // Lightingする場合
+        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        output.color = gMaterial.color * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else
+    { // Lightingしない場合。前回までと同じ演算
+        output.color = gMaterial.color;
+    }
+
     return output;
 }
 
